ddf9c329492365e6c7ea88960ccf0daa3b7e950c
quy2222008 <voquocquy23032022@gmail.com>
2024-06-10 13:51:01 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Csharp_bangcuuchuong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtso = new System.Windows.Forms.TextBox();
+            this.btnxem = new System.Windows.Forms.Button();
+            this.lbsketqua = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(196, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BẢNG CỬU CHƯƠNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(630, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BẠN MUỐN HIỂN THỊ BẢNG CỬU CHƯƠNG MẤY";
+            // 
+            // txtso
+            // 
+            this.txtso.Location = new System.Drawing.Point(108, 101);
+            this.txtso.Multiline = true;
+            this.txtso.Name = "txtso";
+            this.txtso.Size = new System.Drawing.Size(465, 70);
+            this.txtso.TabIndex = 3;
+            // 
+            // btnxem
+            // 
+            this.btnxem.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxem.Location = new System.Drawing.Point(590, 102);
+            this.btnxem.Name = "btnxem";
+            this.btnxem.Size = new System.Drawing.Size(144, 70);
+            this.btnxem.TabIndex = 4;
+            this.btnxem.Text = "xem";
+            this.btnxem.UseVisualStyleBackColor = true;
+            this.btnxem.Click += new System.EventHandler(this.btnxem_Click);
+            // 
+            // lbsketqua
+            // 
+            this.lbsketqua.FormattingEnabled = true;
+            this.lbsketqua.Location = new System.Drawing.Point(127, 229);
+            this.lbsketqua.Name = "lbsketqua";
+            this.lbsketqua.Size = new System.Drawing.Size(565, 186);
+            this.lbsketqua.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbsketqua);
+            this.Controls.Add(this.btnxem);
+            this.Controls.Add(this.txtso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtso;
+        private System.Windows.Forms.Button btnxem;
+        private System.Windows.Forms.ListBox lbsketqua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_bangcuuchuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnxem_Click(object sender, EventArgs e)
+        {
+            lbsketqua.Items.Clear();
+            int cc = Convert.ToInt32(txtso.Text);
+            for (int i = 1; i <=10; i++) 
+            {
+                lbsketqua.Items.Add(txtso.Text + "x" + i.ToString()+"="+Convert.ToString(cc*i));
+            }
+        }
+    }
+}
